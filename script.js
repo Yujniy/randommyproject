651d63cc4a2e76d3349c3efabe729f29f2fa16a0
Yujniy <132089930+Yujniy@users.noreply.github.com>
2023-09-13 21:58:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// Обработчик кнопки активации
+document.getElementById("activate-button").addEventListener("click", function () {
+    const activationKey = document.getElementById("activation-key").value;
+    
+    // Пример проверки ключа активации
+    if (activationKey.toLowerCase() === "тесторобот") {
+        // Если ключ верный, скройте контейнер активации и покажите контейнер генератора
+        document.querySelector(".activation-container").style.display = "none";
+        document.querySelector(".generator-container").style.display = "block";
+    } else {
+        // Если ключ неверный, выведите сообщение об ошибке
+        alert("Неверный ключ активации.");
+    }
+});
+
+// Обработчик кнопки генерации случайных чисел
+document.getElementById("generate-button").addEventListener("click", function () {
+    const minValue = parseInt(document.getElementById("min-value").value);
+    const maxValue = parseInt(document.getElementById("max-value").value);
+    const numResults = parseInt(document.getElementById("num-results").value);
+    const excludeEndpoints = document.getElementById("exclude-endpoints").checked;
+    const unique = document.getElementById("unique").checked;
+
+    if (isNaN(minValue) || isNaN(maxValue) || isNaN(numResults) || numResults > 10) {
+        alert("Пожалуйста, введите корректные значения.");
+        return;
+    }
+
+    if (excludeEndpoints && minValue >= maxValue) {
+        alert("Минимальное значение не может быть больше или равно максимальному.");
+        return;
+    }
+
+    let results = [];
+
+    while (results.length < numResults) {
+        let randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+
+        if (excludeEndpoints && (randomNumber === minValue || randomNumber === maxValue)) {
+            continue;
+        }
+
+        if (unique && results.includes(randomNumber)) {
+            continue;
+        }
+
+        results.push(randomNumber);
+    }
+
+    document.getElementById("result").innerText = results.join(", ");
+});
+
+// Обработчик кнопки копирования результата
+document.getElementById("copy-button").addEventListener("click", function () {
+    const resultText = document.getElementById("result").innerText;
+
+    if (resultText) {
+        const tempInput = document.createElement("input");
+        tempInput.value = resultText;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+        alert("Результат скопирован в буфер обмена: " + resultText);
+    }
+});
